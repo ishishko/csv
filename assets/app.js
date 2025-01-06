5ceddc842a2671cc75cca0755c0cd652a1107c35
Ignacio Shishko <shishkoignacio@gmail.com>
2025-01-06 14:25:06 -0300

    v1.3

--- assets/app.js
+++ assets/app.js
@@ -1,43 +1,36 @@
 // assets/app.js
 
-// Función para cargar y procesar el archivo CSV
-function processCSV() {
-    const filePath = './data/Papua.csv';
+// Función para cargar y procesar el archivo JSON
+function processJSON() {
+    const filePath = './data/data.json';
 
     fetch(filePath)
-        .then(response => response.text())
-        .then(csvText => {
-            Papa.parse(csvText, {
-                header: true,
-                complete: function(results) {
-                    data = results.data;
+        .then(response => response.json())
+        .then(jsonData => {
+            data = jsonData;
 
-                    // Filtrar los datos para eliminar aquellos sin "Scientific name"
-                    data = data.filter(row => row["Scientific name"] && row["Scientific name"].trim() !== '');
+            // Filtrar los datos para eliminar aquellos sin "Scientific name"
+            data = data.filter(row => row["Scientific name"] && row["Scientific name"].trim() !== '');
 
-                    // Ordenar los datos alfabéticamente por el campo "Scientific name"
-                    data.sort((a, b) => {
-                        const nameA = a["Scientific name"].toLowerCase();
-                        const nameB = b["Scientific name"].toLowerCase();
-                        if (nameA < nameB) return -1;
-                        if (nameA > nameB) return 1;
-                        return 0;
-                    });
-
-                    filteredData = data; // Inicializar filteredData con los datos completos
-                    currentPage = 0;
-                    displayPage(currentPage);
-                    updatePaginationControls();
-                },
-                error: function(error) {
-                    console.error('Error al procesar el archivo CSV:', error);
-                }
+            // Ordenar los datos alfabéticamente por el campo "Scientific name"
+            data.sort((a, b) => {
+                const nameA = a["Scientific name"].toLowerCase();
+                const nameB = b["Scientific name"].toLowerCase();
+                if (nameA < nameB) return -1;
+                if (nameA > nameB) return 1;
+                return 0;
             });
+
+            filteredData = data; // Inicializar filteredData con los datos completos
+            const urlParams = new URLSearchParams(window.location.search);
+            currentPage = parseInt(urlParams.get('page')) || 0;
+            displayPage(currentPage);
+            updatePaginationControls();
         })
         .catch(error => {
-            console.error('Error al cargar el archivo CSV:', error);
+            console.error('Error al cargar el archivo JSON:', error);
         });
 }
 
-// Procesar el archivo CSV al cargar la página
-document.addEventListener('DOMContentLoaded', processCSV);
+// Procesar el archivo JSON al cargar la página
+document.addEventListener('DOMContentLoaded', processJSON);
