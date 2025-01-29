8acbeb880b3cdc1a79a7769932cf308723678dfe
Ignacio Shishko <shishkoignacio@gmail.com>
2025-01-29 14:59:49 -0300

    v1.1.React

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
         <h1 className="text-4xl font-bold text-center">Fishes</h1>
       </div>
       <div id="links" className="flex items-center justify-center w-3/4 text-gray-800">
-        {/* <Link to="/" className="p-2 px-8 m-2 text-xl font-bold text-center">Home</Link> */}
+        <Link to="/" className="p-2 px-8 m-2 text-xl font-bold text-center">Home</Link>
         <Link to="/fishes" className="p-2 m-2 text-xl font-bold text-center">Fishes</Link>
         <Link to="/galery" className="p-2 m-2 text-xl font-bold text-center">Galery</Link>
       </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div id="home" className="flex flex-col h-full m-2 p-2">
-            <h1>Welcome to the Home Page</h1>
+        <div id="home" className="flex flex-col p-4 rounded-lg m-auto text-lg font-semibold text-gray-800" >
+            <p>This electronic guide was developed under the NSF DEB-2225130 grant to aid in the identification of freshwater fish species in Papua New Guinea. By compiling data from a variety of trusted sources, we've created a comprehensive, accessible database that you can explore through this guide.
+            The guide includes species from around 40 marine-derived fish families. You can view the full list of these families and explore individual fish species directly on the webpage.</p>
         </div>
         
     );
